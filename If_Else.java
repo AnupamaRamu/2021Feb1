5c8f09048ac7f3bf33d139c91501c16125b2e595
AnupamaRamu <50616522+AnupamaRamu@users.noreply.github.com>
2021-02-01 07:33:06 +0530

    Add files via upload

---
+++ If_Else.java
@@ -0,0 +1,21 @@
+
+public class If_Else {
+
+	public static void main(String[] args) {
+
+		// if-else
+		String weather = "Raining";
+		
+		if(weather == "Raining")
+		{
+			System.out.println("Stay home...");
+		}
+		else
+		{
+			System.out.println("Go for shopping...");
+		}
+		
+		
+	}
+
+}
